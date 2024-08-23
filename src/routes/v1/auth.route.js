9289842b6479420fa94efad249771fd2fdad3a23
Nyamsuren Nyamsuren <nyamsuren.ot@mobicom.mn>
2024-08-23 08:14:56 +0800

    ;

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -5,7 +5,7 @@ const authController = require('../../controllers/auth.controller');
 const router = express.Router();
 
 router.post('/login', validate(authValidation.login), authController.login);
-router.post('/register', validate(authValidation.register), authController.register);
+router.post('/register', authController.register);
 router.post('/logout', validate(authValidation.logout), authController.logout);
 
 module.exports = router;
